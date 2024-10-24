82d15a79b96ef5552404cdfcd28a60cf526de71d
Darrell Wright <beached@users.noreply.github.com>
2024-10-23 20:29:20 -0400

    Initial commit

---
+++ cmake/test_project/test.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/
+//
+
+#include <daw/integer/daw_signed.h>
+
+#include <cstdio>
+
+int main( int argc, char ** argv ) {
+	auto x = daw::i32{5};
+	(void)x;
+}
+
